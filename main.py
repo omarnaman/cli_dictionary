bcf1741ecb3fa675eed4dd70989c3206f5ef4aed
Omar Naman <onaman@uwaterloo.ca>
2021-04-30 11:49:32 -0400

    cleanup the code

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 import sys
-import re
 import requests
 from bs4 import BeautifulSoup
 
-
-SECTION_CLASS = "pgRvse vdBwhd ePtbIe"
-SECTION_JSNAME = "r5Nvmf"
-DEFINITION_CLASS = "L1jWkf h3TRxf"
 
 ALL_CLASS = "Ap5OSd"
 
@@ -27,22 +22,6 @@ class Definition:
     meaning = ""
     example = ""
     synonyms = []
-    def __init__(self, soup=None):
-        if soup is not None:
-            self.get_meaning(soup)
-            self.get_example(soup)
-    
-    def _get_meaning(self, soup):
-        attrs = {"data_dobid": "dfn"}
-        meaning_div = soup.find_all("div", attrs=attrs)
-        if meaning_div is not None:
-            self.meaning = meaning_div[0].text
-    
-    def _get_example(self, soup):
-        attrs = {"class": "H9KYcb"}
-        meaning_div = soup.find_all("div", attrs=attrs)
-        if meaning_div is not None:
-            self.meaning = meaning_div[0].text
 
     def add_synonyms(self, synonyms_text):
         self.synonyms = synonyms_text.split(": ")[1].split(", ")
@@ -67,24 +46,10 @@ class Definition:
 class Section:
     definition_list = None
     title = ""
-    def __init__(self, section_soup=None, title=None):
-        if section_soup is not None:
-            self.get_title(section_soup)
-            self.fill_definitions(section_soup) 
+    def __init__(self, title=None):
         if title is not None:
-            self.title = title   
+            self.title = title.capitalize()
         self.definition_list = []
-    def get_title(self, soup):
-        attrs = {"class": SECTION_CLASS}
-        name_div = soup.find_all("div", attrs=attrs)
-        if name_div is not None:
-            self.title = name_div[0].text
-
-    def fill_definitions(self, soup):
-        attrs = {"class": DEFINITION_CLASS}
-        definition_divs = soup.find_all("div", attrs=attrs)
-        if definition_divs is not None:
-            self.definition_list = map(lambda definition: Definition(definition), definition_divs)
     
     def add_definition(self, definition):
         self.definition_list.append(definition)
@@ -94,17 +59,10 @@ class Section:
         for definition in self.definition_list:
             res += str(definition)
         return res
+
+
+
 def get_meanings(soup):
-    section_attributes = {"jsname": SECTION_JSNAME}
-    sections = soup.find_all("div", attrs=section_attributes)
-    if len(sections) != 0:
-        sections = list(map(lambda section: Section(section), sections))
-        # print(sections)
-    else:
-        raise("Sections not found")
-
-
-def get_meanings_flat(soup):
     spelling_attrs = {"id": "scl"}
     sugg = soup.find_all("a", attrs=spelling_attrs)
     spelling_error = False
@@ -115,7 +73,6 @@ def get_meanings_flat(soup):
     main_div_attrs = {"class": "ZINbbc xpd O9g5cc uUPGi"}
     texts_attrs = {"class": ALL_CLASS}
     inner_attrs = {"class": "BNeawe s3v9rd AP7Wnd"}
-    grey_inner_attrs = {"class": "r0bn4c rQMQod"}
     first_div = soup.find_all("div", attrs=main_div_attrs)
     first_div = first_div[int(spelling_error)]
     outer_texts = first_div.find_all("div", attrs=texts_attrs)
@@ -158,28 +115,13 @@ def get_meanings_flat(soup):
 
 def get_html_soup(url):
     html_text = requests.get(url).text
-    with open("out2.html", "w") as f:
-        f.write(html_text)
     soup = BeautifulSoup(html_text, 'html.parser')
     return soup
 
 def print_word_meaning(word):
     url = f"https://www.google.com/search?client=firefox-b-d&q=define+{word}"
     soup = get_html_soup(url)
-    meanings = get_meanings_flat(soup)
-    # print_meanings(meanings)
-
-# def print_meanings(meanings):
-#     attrs = {
-#             "class": LUNA_LABEL_CLASS
-#             }
-#     for i, meaning in enumerate(meanings):
-#         title = meaning.find('span', attrs=attrs)
-#         if title is not None:
-#             print(f"\n{TITLE_COLOR}{title.contents[0]}{RESET_COLOR}:")
-#         else:
-#             text = ''.join(extract_text(meaning))
-#             print(f"{COLORS[i%2]}{text}{RESET_COLOR}")
+    get_meanings(soup)
 
 def main(word):
     print_word_meaning(word)
