e1ef262400d83ccaf8361fd74215b0986417908c
Omar Naman <onaman@uwaterloo.ca>
2021-04-30 10:55:14 -0400

    Fix errors parsing google search result\nAdd spelling suggestion

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ RESET_COLOR = '\033[0m'
 class Definition:
     meaning = ""
     example = ""
+    synonyms = []
     def __init__(self, soup=None):
         if soup is not None:
             self.get_meaning(soup)
@@ -37,19 +38,24 @@ class Definition:
         if meaning_div is not None:
             self.meaning = meaning_div[0].text
 
+    def add_synonyms(self, synonyms_text):
+        self.synonyms = synonyms_text.split(": ")[1].split(", ")
+
     def __str__(self):
         return f"Meaning: {self.meaning}\nExample: {self.example}"
 
     def __repr__(self):
-        return f"Meaning: {self.meaning}\nExample: {self.example}"
+        return f"Meaning: {self.meaning}\nExample: {self.example}\nSynonyms: {self.synonyms}"
 
 class Section:
     definition_list = None
     title = ""
-    def __init__(self, section_soup=None):
+    def __init__(self, section_soup=None, title=None):
         if section_soup is not None:
             self.get_title(section_soup)
-            self.fill_definitions(section_soup)    
+            self.fill_definitions(section_soup) 
+        if title is not None:
+            self.title = title   
         self.definition_list = []
     def get_title(self, soup):
         attrs = {"class": SECTION_CLASS}
@@ -63,67 +69,73 @@ class Section:
         if definition_divs is not None:
             self.definition_list = map(lambda definition: Definition(definition), definition_divs)
     
+    def add_definition(self, definition):
+        self.definition_list.append(definition)
+
     def __repr__(self) -> str:
-        return f"Title: {self.title}\nDefinitions:\n{self.definition_list}"
+        return f"Type: {self.title}\nDefinitions:\n{self.definition_list}"
 
 def get_meanings(soup):
     section_attributes = {"jsname": SECTION_JSNAME}
     sections = soup.find_all("div", attrs=section_attributes)
     if len(sections) != 0:
         sections = list(map(lambda section: Section(section), sections))
-        print(sections)
+        # print(sections)
     else:
         raise("Sections not found")
 
 
 def get_meanings_flat(soup):
+    spelling_attrs = {"id": "scl"}
+    sugg = soup.find_all("a", attrs=spelling_attrs)
+    spelling_error = False
+    if len(sugg) != 0:
+        fixs = sugg[0].find_all("i")
+        spelling_error = True
+        print(" ".join([fix.text for fix in fixs]))
+    main_div_attrs = {"class": "ZINbbc xpd O9g5cc uUPGi"}
     texts_attrs = {"class": ALL_CLASS}
     inner_attrs = {"class": "BNeawe s3v9rd AP7Wnd"}
     grey_inner_attrs = {"class": "r0bn4c rQMQod"}
-    outer_texts = soup.find_all("div", attrs=texts_attrs)
-    temp_section = None
-    sections = []
-    for outer_text in outer_texts:
-        inner_divs = outer_text.find_all("div", attrs=inner_attrs)
-        inner_count = len(inner_divs)
-        if inner_count == 0:
-            continue
-        if inner_count == 1:
-            if temp_section is not None:
-                if temp_section.title == "" or len(temp_section.definition_list) == 0:
-                    pass
-                else:
-                    sections.append(temp_section)
-            temp_section = Section()
-            inner_div = inner_divs[0]
-            grey_div = inner_div.find_all("span", attrs=grey_inner_attrs)
-            if len(grey_div) != 0:
-                temp_section.title = grey_div[0].text
-        if inner_count > 1:
-            meaning = ""
-            example = ""
-            for inner_div in inner_divs:
-                grey_div = inner_div.find_all("span", attrs=grey_inner_attrs)
-                if len(grey_div) == 0:
-                    text = inner_div.text
-                    if len(text) > 1:
-                        meaning = text
-                else:
-                    text = grey_div[0].text
-                    example = text
-                if len(meaning) > 1 and len(example) > 1:
-                    temp_def = Definition()
-                    temp_def.example = example
-                    temp_def.meaning = meaning
-                    temp_section.definition_list.append(temp_def)
-                    
-    if temp_section is not None:
-        if temp_section.title == "" or len(temp_section.definition_list) == 0:
-            pass
-        else:
-            sections.append(temp_section)
-    [print(section, "\n") for section in sections]
+    first_div = soup.find_all("div", attrs=main_div_attrs)
+    first_div = first_div[int(spelling_error)]
+    outer_texts = first_div.find_all("div", attrs=texts_attrs)
+    inner_texts = []
+    for div in outer_texts:
+        inner_texts += div.find_all("div", attrs=inner_attrs)
 
+    temp_section = None
+    temp_def = None
+    sections = []
+    texts = [div.text for div in inner_texts]
+    for text in texts:
+        if len(text) <=1 :
+            continue
+        num_words = len(text.split())
+        if num_words == 1: # Title
+            if temp_section is not None:
+                if temp_def is not None:
+                    temp_section.add_definition(temp_def)
+                sections.append(temp_section)
+            temp_section = Section(title=text)
+            temp_def = None
+        elif num_words > 1: # Meaning or Example
+            if text.startswith("\"") and text.endswith("\"") or text.startswith("'") and text.endswith("'"):
+                temp_def.example = text
+            elif text.startswith("synonyms:"):
+                temp_def.add_synonyms(text)
+                
+            else:
+                if temp_def is not None:
+                    temp_section.add_definition(temp_def)
+                temp_def = Definition()
+                temp_def.meaning = text
+        
+    if temp_section is not None:
+            if temp_def is not None:
+                temp_section.add_definition(temp_def)
+            sections.append(temp_section)
+    [print(section) for section in sections]
 
 def get_html_soup(url):
     html_text = requests.get(url).text
